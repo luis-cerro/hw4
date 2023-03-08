51880755e1f58c21bd74d3048564464ec13df1f3
Luis Cerro <luis.cerro@kellogg.northwestern.edu>
2023-03-08 21:12:26 +0000

    commit_3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # authenticate the user
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["username"]}."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Nope."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Nope."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    flash["notice"] = "You are being logged out."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
